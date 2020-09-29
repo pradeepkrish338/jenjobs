f64a5dccac50c7f207596c1e47e7762ddadf7ac8
pradeepkrish338 <71810983+pradeepkrish338@users.noreply.github.com>
2020-09-29 11:05:01 +0530

    first git java program

---
+++ Sample.java
@@ -0,0 +1,12 @@
+/*import java.io.*;*/
+
+public class Sample
+{
+	public static void main(String args[])
+	{
+		for (int i=1; i<=10;i++)
+		{
+			System.out.println("Hello world >> Count :" + i);
+		}
+	}
+}
